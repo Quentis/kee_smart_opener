b471591e733942b97fecda2753cdf23bfecaae22
Quentis Ghyll <quentisgh@gmail.com>
2026-08-15 14:56:35 +0200

    Create KSO Zephyr project with RPi Pico Project extension

---
+++ sw/kso-z/src/main.c
@@ -0,0 +1,16 @@
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
+
+int main(void) {
+    printk("Hello World from Pico!\n");
+
+    while (1) {
+        printk("Running on %s...\n", CONFIG_BOARD);
+
+        k_sleep(K_MSEC(1000));
+    }
+
+    return 0;
+}
